a3d98e2dd56c30e35ba1961a8b44d0b28d5977f1
Lars Eckart <lars.eckart@fortumo.com>
2016-10-19 21:40:10 +0300

    cleanup build.gradle, write failing first test

---
+++ src/main/java/ee/tublipoiss/jbrains/AnswerMachine.java
@@ -0,0 +1,8 @@
+package ee.tublipoiss.jbrains;
+
+public class AnswerMachine {
+
+    public int answer() {
+        throw new UnsupportedOperationException("answer() not implemented yet");
+    }
+}
