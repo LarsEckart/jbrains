814ace8d7fa83f040ac99ea3791ff82aeabaaa56
Lars Eckart <lars.eckart@fortumo.com>
2016-10-21 23:00:19 +0300

    adds mutation testing capability + tests for hashcode, just for completeness.

--- src/test/java/ee/tublipoiss/jbrains/HashCodeTest.java
+++ src/test/java/ee/tublipoiss/jbrains/HashCodeTest.java
@@ -26,60 +26,38 @@ package ee.tublipoiss.jbrains;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-public class FractionEqualsTest {
-
+public class HashCodeTest {
+    
     @Test
-    public void differentObject() throws Exception {
+    public void hashcodeCalculatedCorrectly() throws Exception {
         // given
-
+            
         // when
-
+            
         // then
-        assertNotEquals(new ArrayList<String>(), new Fraction(3, 5));
+        assertEquals(95, new Fraction(3,2).hashCode());
     }
 
     @Test
-    public void sameNumeratorAndDenominator() throws Exception {
+    public void sameHashCodeForSameFractions() throws Exception {
         // given
 
         // when
 
         // then
-        assertEquals(new Fraction(3, 5), new Fraction(3, 5));
+        assertEquals(new Fraction(3,2).hashCode(), new Fraction(3,2).hashCode());
     }
 
     @Test
-    public void sameNumeratorAndDifferentDenominator() throws Exception {
+    public void differentHashCodeForDifferentFractions() throws Exception {
         // given
 
         // when
 
         // then
-        assertNotEquals(new Fraction(3, 5), new Fraction(3, 6));
-    }
-
-    @Test
-    public void differentNumeratorAndSameDenominator() throws Exception {
-        // given
-
-        // when
-
-        // then
-        assertNotEquals(new Fraction(3, 5), new Fraction(4, 5));
-    }
-
-    @Test
-    public void numeratorAndDenominatorMultipleOf() throws Exception {
-        // given
-
-        // when
-
-        // then
-        assertEquals(new Fraction(3, 5), new Fraction(6, 10));
+        assertNotEquals(new Fraction(2,2).hashCode(), new Fraction(3,2).hashCode());
     }
 }
